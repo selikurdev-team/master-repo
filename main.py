d77be80f3c1eecee5e592512fcd9f27cc307b158
selikurdev-team <selikurdev@gmail.com>
2023-08-26 23:40:32 +0700

    add commentar to describe the project

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+"""
+Ini adalah demo project pertama, dari instalasi pycharm,
+sampai menghubungkan ke github account, bekerja dengan virtualenv
+dan project python sudah siap untuk dilanjutkan
+"""
 print("this is first step to mastering python")
 print("selikurdev go remote!")
